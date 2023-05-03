003c5e7e8906aba2b1f615856139fcc52132fc4b
NicoMateo <nico_mateo1982@hotmail.com>
2023-05-03 09:40:49 -0300

    uevo Carro de Compras

--- carro.js
+++ carro.js
@@ -1,61 +1,34 @@
-// Objeto para almacenar los productos del carro de compras
-var carroDeCompras = {};
+let carroDeCompras = [];
 
-// Función para agregar un producto al objeto del carro de compras
-function agregarProducto(producto, precio, cantidad) {
-    if (carroDeCompras[producto]) {
-        carroDeCompras[producto].cantidad += cantidad;
-    } else {
-        carroDeCompras[producto] = {
-            precio: precio,
-            cantidad: cantidad
-        };
+function agregarAlCarro(producto, cantidad) {
+    let encontrado = false;
+    for (const element of carroDeCompras) {
+        if (element.producto === producto) {
+        element.cantidad += cantidad;
+        encontrado = true;
+        break;
+        }
     }
-    actualizarTabla();
+        if (!encontrado){
+        carroDeCompras.push({producto: producto, cantidad: cantidad});
+        }
 }
 
-// Función para eliminar un producto del objeto del carro de compras
-function eliminarProducto(producto) {
-    delete carroDeCompras[producto];
-    actualizarTabla();
+function calcularPrecioTotal() {
+    let total = 0;
+    for (const element of carroDeCompras) {
+        let precio = productos[element.producto].precio;
+        let cantidad = element.cantidad;
+        if (productos[element.producto].descuento) {
+        let descuento = productos[element.producto].descuento;
+        total += precio * cantidad * (1 - descuento/100);
+        } else {
+        total += precio * cantidad;
+        }
+    }
+    return total;
 }
 
-// Función para actualizar la tabla HTML con los productos del carro de compras
-function actualizarTabla() {
-    var tabla = document.getElementById("carro").getElementsByTagName("tbody")[0];
-    tabla.innerHTML = "";
-    for (var producto in carroDeCompras) {
-        var fila = tabla.insertRow(-1);
-        var celdaProducto = fila.insertCell(0);
-        var celdaPrecio = fila.insertCell(1);
-        var celdaCantidad = fila.insertCell(2);
-        var celdaTotal = fila.insertCell(3);
-        var celdaEliminar = fila.insertCell(4);
-        celdaProducto.innerHTML = producto;
-        celdaPrecio.innerHTML = carroDeCompras[producto].precio;
-        celdaCantidad.innerHTML = carroDeCompras[producto].cantidad;
-        celdaTotal.innerHTML = carroDeCompras[producto].precio * carroDeCompras[producto].cantidad;
-        celdaEliminar.innerHTML = '<button onclick="eliminarProducto(\'' + producto + '\')">Eliminar</button>';
-    }
+function vaciarCarro() {
+    carroDeCompras = [];
 }
-
-// Evento para agregar un producto al carro de compras
-document.getElementById("agregarProducto").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var producto = document.getElementById("producto").value;
-    var precio = parseFloat(document.getElementById("precio").value);
-    var cantidad = parseInt(document.getElementById("cantidad").value);
-    agregarProducto(producto, precio, cantidad);
-    document.getElementById("agregarProducto").reset();
-});
-
-// Evento para finalizar la compra
-document.getElementById("finalizarCompra").addEventListener("click", function() {
-    var total = 0;
-    for (var producto in carroDeCompras) {
-        total += carroDeCompras[producto].precio * carroDeCompras[producto].cantidad;
-    }
-    alert("Total de la compra: $" + total);
-    carroDeCompras = {};
-    actualizarTabla();
-});
